0fb036a4fdb64115f38c93cd05327427f20c7ec7
Doaeem <doaemouden@gmail.com>
2022-06-04 15:31:45 +0000

    Initial commit

---
+++ app/src/main/java/com/example/ex2json/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.ex2json;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lv=findViewById(R.id.lst);
+
+        String json = loadJsonFromRaw(R.raw.stagiaire);
+
+        try {
+            JSONArray arr = new JSONArray(json);
+            ArrayList<String> res = new ArrayList<>();
+
+            for(int i=0;i<arr.length();i++){
+                JSONObject ob = arr.getJSONObject(i);
+
+                res.add(ob.getString("nom") + "-" + ob.getString("filiere") + " - " + ob.getInt("age"));
+            }
+
+            ArrayAdapter ad = new ArrayAdapter(this, android.R.layout.simple_list_item_1,res);
+            lv.setAdapter(ad);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+    public String loadJsonFromRaw(int resId) {
+        try {
+            InputStream in = getResources().openRawResource(resId);
+            byte[] data = new byte[in.available()];
+            in.read(data);
+            in.close();
+            return new String(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+}
